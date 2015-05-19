44d330a8c7d4943ce6fd241f7e7042e3ddfa6bc4
Sergej Dugokontov <sergejspam@gmail.com>
2015-05-19 13:33:51 +0200

    Add sufix Store for Store classes. Use store name for actions name.

--- store/templates/store.js
+++ store/templates/store.js
@@ -1,6 +1,6 @@
 import GLU from 'glu.js';
 
-class <%= storeName %> extends GLU.Store {
+class <%= storeName %>Store extends GLU.Store {
     constructor() {
         super();
 
@@ -8,8 +8,8 @@ class <%= storeName %> extends GLU.Store {
         // listen for actions
 
         this.bindActions(
-            ExampleActions.ACTION, this.actionHandler, [],
-            ExampleActions.ACTION_2, this.actionTwoHandler, []
+            <%= storeName %>Actions.ACTION, this.actionHandler, [],
+            <%= storeName %>Actions.ACTION_2, this.actionTwoHandler, []
         );
         */
     }
@@ -26,4 +26,4 @@ class <%= storeName %> extends GLU.Store {
     */
 }
 
-export default new <%= storeName %>();
+export default new <%= storeName %>Store();
